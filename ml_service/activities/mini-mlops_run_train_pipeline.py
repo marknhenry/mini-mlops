5c5768836b6d1a6438458e351a4477ab858ca416
Mark Henry <marknhenry@live.com>
2021-06-01 22:45:53 +0400

    Issue with Version

--- ml_service/activities/mini-mlops_run_train_pipeline.py
+++ ml_service/activities/mini-mlops_run_train_pipeline.py
@@ -31,31 +31,38 @@ def main():
             matched_pipes.append(p)
             # print('Found pipeline: ', p)
     
-    if(len(matched_pipes) > 1):
-        published_pipeline = None
-        raise Exception(f"Multiple active pipelines are published for build {e.build_id}.")  # NOQA: E501
-    elif(len(matched_pipes) == 0):
+
+    # Because of debugging, there are many pipelines, and so I'm commenting this part out: 
+    # if(len(matched_pipes) > 1):
+    #     published_pipeline = None
+    #     raise Exception(f"Multiple active pipelines are published for build {e.build_id}.")  # NOQA: E501
+    
+    if(len(matched_pipes) == 0):
         published_pipeline = None
         raise KeyError(f"Unable to find a published pipeline for this build {e.build_id}")  # NOQA: E501
     else:
         published_pipeline = matched_pipes[0]
         print("published pipeline id is", published_pipeline.id)
 
-         # Save the Pipeline ID for other AzDO jobs after script is complete
+        # Save the Pipeline ID for other AzDO jobs after script is complete
         if args.output_pipeline_id_file is not None:
             with open(args.output_pipeline_id_file, "w") as out_file:
                 out_file.write(published_pipeline.id)
 
         
-        pipeline_parameters = {'model_name': e.model_name}
+        # Also exluded
+        # So the run is included here, but it is split into another step, where the pipeline
+        # in the yml file is running it directly as tasks from DevOps.  
 
-        tags = {'BuildID': e.build_id}
-        if (e.build_uri is not None):
-            tags['BuildUri'] = e.build_uri
+        # pipeline_parameters = {'model_name': e.model_name}
+
+        # tags = {'BuildID': e.build_id}
+        # if (e.build_uri is not None):
+        #     tags['BuildUri'] = e.build_uri
         
-        experiment = Experiment(workspace=aml_workspace, name=e.experiment_name)
+        # experiment = Experiment(workspace=aml_workspace, name=e.experiment_name)
 
-        run = experiment.submit(published_pipeline, tags=tags, pipeline_parameters=pipeline_parameters)
+        # run = experiment.submit(published_pipeline, tags=tags, pipeline_parameters=pipeline_parameters)
 
 if __name__ == '__main__':
     main()
